013a3045d62fda572ca3955aaf54d0337225f0f7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-18 22:04:32 +0300

    Create Canva_images.js

---
+++ Canva_images.js
@@ -0,0 +1,24 @@
+
+
+How I have canvas, a website for designers, I have this otiopn on canva where I can Customise the size of a design to a size of like e.g 1240 by 900, this increase the image in size rather than sclaing it on a website, Is this approach good  
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+  Customizing the size of a design in Canva to a specific dimension, such as 1240 by 900 pixels, can be a valid approach depending on your specific use case. Here are some considerations to help you determine if this approach is suitable for your website:
+
+Image Quality: By designing your image at the desired dimensions upfront, you can ensure that it maintains optimal quality. Scaling down an image often results in a reduction in quality, while scaling up can lead to pixelation and blurriness. Designing at the intended size from the beginning can help preserve image clarity.
+
+Artistic Control: Designing the image at a specific size allows you to have more control over the visual composition and layout. You can precisely position elements and adjust their sizes to fit within the desired dimensions without relying on automatic scaling.
+
+Responsive Considerations: Its important to consider the responsiveness of your website. If you design an image at a fixed size, it may not adapt well to different screen sizes. Ensure that your website's layout and design are responsive and can handle images of various dimensions without causing issues on smaller screens or mobile devices.
+
+File Size: Larger images with specific dimensions can result in larger file sizes. This can affect website performance, particularly for users with slower internet connections. Be mindful of optimizing your images for the web to strike a balance between image quality and file size.
+
+Flexibility: Designing at a fixed size can limit the versatility of your image. If you need to reuse the image in different contexts or adapt it to different layouts, designing at a flexible size or providing alternative versions may be more beneficial.
+
+Ultimately, the suitability of designing at a specific size in Canva depends on the specific requirements and constraints of your project. Consider factors such as image quality, responsiveness, file size, and flexibility when deciding on the best approach for your website.
+
+
+
+
+
