eba9b8920c7f6e1016f278de0b170baed6c90a0c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-18 21:32:00 +0300

    Create Back-Ground_attachment.js

---
+++ Back-Ground_attachment.js
@@ -0,0 +1,43 @@
+Background Attachment
+Utilities for controlling how a background image behaves when scrolling.
+
+​
+Quick reference
+
+bg-fixed	                                      background-attachment: fixed;
+bg-local	                                      background-attachment: local;
+bg-scroll                                     	background-attachment: scroll;
+
+
+
+
+
+
+
+
+1.Fixed
+Use bg-fixed to fix the background image relative to the viewport.
+
+  <div class="bg-fixed ..." style="background-image: url(...)"></div>
+
+
+2.Local
+Use bg-local to scroll the background image with the container and the viewport.
+
+<div class="bg-local ..." style="background-image: url(...)"></div>
+
+
+
+3.Scroll
+Use bg-scroll to scroll the background image with the viewport, but not with the container.
+
+<div class="bg-scroll ..." style="background-image: url(...)"></div>
+
+  
+
+
+
+
+
+
+
