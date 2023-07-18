5056ed6697adbf2f7f17b8fbc0bc45d0d5212f84
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-18 16:28:14 +0300

    Create Max-width.py

---
+++ Max-width.py
@@ -0,0 +1,12 @@
+Definition and Usage
+The max-width property defines the maximum width of an element.
+
+If the content is larger than the maximum width, it will automatically change the height of the element.
+
+If the content is smaller than the maximum width, the max-width property has no effect.
+
+Note: This prevents the value of the width property from becoming larger than max-width. The value of the max-width property overrides the width property.
+
+
+
+
