366790dec9d05ceb941b958f05fb3c4cae872e31
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-08 20:03:01 +0300

    Create Image_responsive.java

---
+++ 1_Screen_sizes_I_recommend/Image_responsive.java
@@ -0,0 +1,4 @@
+<div className="e h-96 w-[60%] sm:shrink-0 bg-slate-50 md:flex">
+<img class="h-48  w-full object-cover object-center md:h-full md:w-48" src="/img/grocery-image.png" alt="Modern building architecture"/>
+<p> Helloooo Georgge and wlelell</p>
+</div>
