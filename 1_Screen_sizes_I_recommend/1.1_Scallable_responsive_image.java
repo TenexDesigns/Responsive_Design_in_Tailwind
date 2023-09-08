cc9e86a057422112b24d5a78c30f3a198187833d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-08 20:40:11 +0300

    Create 1.1_Scallable_responsive_image.java

---
+++ 1_Screen_sizes_I_recommend/1.1_Scallable_responsive_image.java
@@ -0,0 +1,10 @@
+
+This is a responisve image in a responsive container
+
+
+
+<div className="e h-[50%] w-[60%]  bg-slate-50 ">
+
+<img class="h-full  w-full object-cover " src="/img/grocery-image.png" alt="Modern building architecture"/>
+
+</div>
